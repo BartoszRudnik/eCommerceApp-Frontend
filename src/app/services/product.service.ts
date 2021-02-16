4ed0fab643a6f382e8b9416a2215e98264be23cc
Bartosz Rudnik <rudnik49@gmail.com>
2021-02-16 15:19:14 +0100

    search for products by category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,9 +13,11 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { };
 
-  getProductList(): Observable<Product[]>{
+  getProductList(categoryId: number): Observable<Product[]>{
 
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response => response._embedded.products));
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(map(response => response._embedded.products));
 
   }
 
