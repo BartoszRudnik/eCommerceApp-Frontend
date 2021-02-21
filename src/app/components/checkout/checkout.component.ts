ac932d1236f7495d3ba4a56042811698e1d2901e
Bartosz Rudnik <rudnik49@gmail.com>
2021-02-21 19:17:41 +0100

    populate year and month for credit card form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ShopFormService } from 'src/app/services/shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +14,10 @@ export class CheckoutComponent implements OnInit {
   totalPrice : number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardMonths: number[] = [];
+  creditCardYears: number[] = [];
+
+  constructor(private formBuilder: FormBuilder, private shopFormService: ShopFormService) { }
 
   ngOnInit(): void {
 
@@ -59,6 +63,25 @@ export class CheckoutComponent implements OnInit {
       })
 
     });
+
+    let currentMonth: number = new Date().getMonth() + 1;
+
+    this.shopFormService.getCreditCardMonths(currentMonth).subscribe(
+
+      data => {
+        this.creditCardMonths = data;
+      }
+
+    );
+
+    this.shopFormService.getCreditCardYears().subscribe(
+
+      data => {
+        this.creditCardYears = data;
+      }      
+
+    );
+
 
   }
 
