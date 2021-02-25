b1332f071e608bf8f0f0c2c04ae2f88ed4037f2a
Bartosz Rudnik <rudnik49@gmail.com>
2021-02-25 11:09:55 +0100

    okta login/logout

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,12 @@
+export default {
+
+    oidc: {
+
+        clientId: '0oa9833f4zVNIHCeQ5d6',
+        issuer: 'https://dev-59845183.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+
+    }
+
+}
